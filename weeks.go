6ab8d83ff0379f473842b8c0f952f64f2874a3f0
Marcelo Monaco <marcelo.monaco@gmail.com>
2018-09-26 17:32:53 -0300

    New functions: LastDayOfWeek & NextDayOfWeek

--- weeks.go
+++ weeks.go
@@ -2,6 +2,42 @@ package timex
 
 import "time"
 
+func LastDayOfWeek(t time.Time, weekday time.Weekday) (day time.Time) {
+
+	day = t
+
+	for {
+
+		if day.Weekday() == weekday {
+			break
+		}
+
+		day = SubDays(day,1)
+
+	}
+
+	return
+
+}
+
+func NextDayOfWeek(t time.Time, weekday time.Weekday) (day time.Time) {
+
+	day = t
+
+	for {
+
+		if day.Weekday() == weekday {
+			break
+		}
+
+		day = AddDays(day,1)
+
+	}
+
+	return
+
+}
+
 func WeeksBreakdown(t1, t2 time.Time) (dates []time.Time) {
 
 	dates = append(dates, t1)
